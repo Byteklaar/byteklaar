537bf03de8bccdacd78a9492eb3a9d18d0ea9356
bramdupont <bram@dupontwebdesign.com>
2024-04-21 15:43:01 +0200

    Project gallerij logic

--- frontend/src/app/werk/page.js
+++ frontend/src/app/werk/page.js
@@ -1,20 +1,11 @@
 import {getProjectenData} from "@/data/loaders";
 import {ProjectenGallerij} from "@/components/custom/Werk/ProjectenGallerij";
 
-function blockRenderer(block) {
-    switch (block.__component) {
-        case "layout.hero-section":
-            return <ProjectenGallerij key={block.id} data={block}/>;
-        default:
-            return null;
-    }
-}
-
 export default async function Page() {
-    const strapiData = await getProjectenData();
+    const projectData = await getProjectenData();
+    console.log(projectData.data);
 
-    const {blocks} = strapiData;
-    if (!blocks) return <p>No sections found</p>;
-
-    return <ProjectenGallerij key={blocks.id} data={blocks}/>;
+    return (
+        <ProjectenGallerij data={projectData.data} />
+    )
 }
--- frontend/src/components/custom/Werk/ProjectenGallerij.js
+++ frontend/src/components/custom/Werk/ProjectenGallerij.js
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 export function ProjectenGallerij({data}) {
-    const {titel, beschrijving, extra_beschrijving, gallerij} = data;
     return (
         <div className="flex row">
-            <div className="basis-3/5 px-16 my-16">
-                <h2 className="text-8xl w-10/12">{titel}</h2>
-            </div>
-            <div className="basis-2/5 bg-byte-beige py-20 text-xl px-16">
-            </div>
+            {data.map((project) => (
+                <div key={project.id}>
+                    <h2 className="text-8xl w-10/12">{project.titel}</h2>
+                    <p className="text-xl w-10/12">{project.beschrijving}</p>
+                </div>
+            ))}
         </div>
     );
 }
