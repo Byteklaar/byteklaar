f0972933a166b8e15a2a30e2429353cdce8f40d9
bramdupont <bram@dupontwebdesign.com>
2024-07-09 17:30:42 +0200

    Added submit state

--- frontend/src/components/custom/Contact/ContactForm.js
+++ frontend/src/components/custom/Contact/ContactForm.js
@@ -1,3 +1,4 @@
+'use client';
 import {useEffect} from 'react';
 
 export function ContactForm({data}) {
@@ -10,6 +11,7 @@ export function ContactForm({data}) {
         email: "",
         message: ""
     });
+    const [formSuccess, setFormSuccess] = useState(false)
 
     const handleInput = (e) => {
         const fieldName = e.target.name;
@@ -45,6 +47,7 @@ export function ContactForm({data}) {
                 email: "",
                 message: ""
             })
+            setFormSuccess(true)
         })
 
         // let voornaam = document.getElementsByName('Voornaam')[0].value;
@@ -67,45 +70,51 @@ export function ContactForm({data}) {
     }
 
     return (
-        <form method="post" action="https://app.loops.so/api/newsletter-form/clw3p9dyl039ts88steniolch"
-              onSubmit={handleSubmit}
-              className="flex flex-col lg:grid lg:grid-flow-row lg:grid-cols-2 gap-x-8 gap-y-3">
-            <input type="hidden" name="userGroup" value="Website contactForm"/>
-            {data.field.map((field) => (
-                field.type === "textarea" ? (
-                    <textarea
-                        className="border-2 col-start-2 row-start-2 row-span-3 placeholder:text-white text-white py-2 px-6 border-white rounded-3xl bg-transparent"
-                        key={field.id}
-                        placeholder={field.naam}
-                        rows={5}
-                        required={field.verplicht}
-                        name={field.naam}
-                    />
-                ) : (
-                    <input
-                        className="border-2 col-start-1 col-end-1 placeholder:text-white text-white py-2 px-6 border-white rounded-full bg-transparent"
-                        key={field.id}
-                        type={field.type}
-                        placeholder={field.naam}
-                        required={field.verplicht}
-                        name={field.naam}
-                        onChange={handleInput}
-                    />
-                )
-            ))}
-            <select
-                className="col-start-2 col-end-2 row-start-1 border-2 appearance-none outline-none placeholder:text-white text-white py-2 px-6 border-white rounded-3xl bg-transparent"
-                name="Fase" id="Fase" required={data.dropdownVerplicht}>
-                <option defaultValue>{data.dropdownDefault}</option>
-                {data.dropdown.map((field) => (
-                    <option key={field.id} value={field.titel}>{field.titel}</option>
-                ))}
-            </select>
-            <button
-                className="col-start-2 col-end-2 hover:bg-white hover:text-byte-blue cursor-pointer ease-in-out duration-150 p-2 border-2 border-white text-white rounded-full block"
-                type="submit"
-            >Versturen
-            </button>
-        </form>
+        <div>
+            {formSuccess ?
+                <div>Formulier succesvol verzonden.</div>
+                :
+                <form method="post" action="https://app.loops.so/api/newsletter-form/clw3p9dyl039ts88steniolch"
+                      onSubmit={handleSubmit}
+                      className="flex flex-col lg:grid lg:grid-flow-row lg:grid-cols-2 gap-x-8 gap-y-3">
+                    <input type="hidden" name="userGroup" value="Website contactForm"/>
+                    {data.field.map((field) => (
+                        field.type === "textarea" ? (
+                            <textarea
+                                className="border-2 col-start-2 row-start-2 row-span-3 placeholder:text-white text-white py-2 px-6 border-white rounded-3xl bg-transparent"
+                                key={field.id}
+                                placeholder={field.naam}
+                                rows={5}
+                                required={field.verplicht}
+                                name={field.naam}
+                            />
+                        ) : (
+                            <input
+                                className="border-2 col-start-1 col-end-1 placeholder:text-white text-white py-2 px-6 border-white rounded-full bg-transparent"
+                                key={field.id}
+                                type={field.type}
+                                placeholder={field.naam}
+                                required={field.verplicht}
+                                name={field.naam}
+                                onChange={handleInput}
+                            />
+                        )
+                    ))}
+                    <select
+                        className="col-start-2 col-end-2 row-start-1 border-2 appearance-none outline-none placeholder:text-white text-white py-2 px-6 border-white rounded-3xl bg-transparent"
+                        name="Fase" id="Fase" required={data.dropdownVerplicht}>
+                        <option defaultValue>{data.dropdownDefault}</option>
+                        {data.dropdown.map((field) => (
+                            <option key={field.id} value={field.titel}>{field.titel}</option>
+                        ))}
+                    </select>
+                    <button
+                        className="col-start-2 col-end-2 hover:bg-white hover:text-byte-blue cursor-pointer ease-in-out duration-150 p-2 border-2 border-white text-white rounded-full block"
+                        type="submit"
+                    >Versturen
+                    </button>
+                </form>
+            }
+        </div>
     );
 }
