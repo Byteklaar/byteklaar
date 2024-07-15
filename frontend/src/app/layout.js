aeae4b58a61750122fd2b5dd8517158b3817d8bf
bramdupont <bram@dupontwebdesign.com>
2024-07-15 18:52:52 +0200

    Updated animation for Safari

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -26,8 +26,8 @@ export default async function RootLayout({children}) {
         <Script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/ScrollTrigger.min.js"
                 strategy="beforeInteractive"/>
         <Script src="https://dfrnc.com/lib/SplitText.min.js" strategy="beforeInteractive"/>
-        <Script id="Leadinfo-Script">
-            {`function leadinfo(l, e, a, d, i, n, f, o) {
+        <Script id="Leadinfo-Script" strategy="beforeInteractive">
+            {`function leadinfo (l, e, a, d, i, n, f, o) {
                 if (!l[i]) {
                     l.GlobalLeadinfoNamespace = l.GlobalLeadinfoNamespace || [];
                     l.GlobalLeadinfoNamespace.push(i);
