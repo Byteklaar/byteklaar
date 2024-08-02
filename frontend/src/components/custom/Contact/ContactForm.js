87919cfc34dcc75f17a30f1a169a80fcb3ff98f5
bramdupont <bram@dupontwebdesign.com>
2024-08-02 14:23:26 +0200

    Added contact form

--- frontend/src/components/custom/Contact/ContactForm.js
+++ frontend/src/components/custom/Contact/ContactForm.js
@@ -1,14 +1,14 @@
-'use client';
-import {emailHandler} from './emailHandler';
-import {useState} from "react";
+"use client";
+import { sendForm } from "./emailHandler.js";
+import { useState } from "react";
 
-export function ContactForm({data}) {
+export function ContactForm({ data }) {
     const [formSuccess, setFormSuccess] = useState(false);
 
     const [formData, setFormData] = useState({
         name: "",
         email: "",
-        message: ""
+        message: "",
     });
 
     const handleInput = (e) => {
@@ -17,20 +17,39 @@ export function ContactForm({data}) {
 
         setFormData((prevState) => ({
             ...prevState,
-            [fieldName]: fieldValue
+            [fieldName]: fieldValue,
         }));
-    }
+    };
+
+    const handleSubmit1 = async (e, date) => {
+        let customText = `Text ${date} date`;
+        e.preventDefault();
+        try {
+            let isSent = await sendForm(customText);
+
+            if (isSent) {
+                setFormSuccess(true);
+            } else {
+                setFormSuccess(false);
+            }
+        } catch (error) {
+            setFormSuccess(false);
+            console.error("Error in handleSubmit1:", error);
+        } // handleSubmit(e, setFormSuccess); // Асинхронний виклик
+    };
 
     return (
         <div>
-            {formSuccess ?
+            {formSuccess ? (
                 <div>Formulier succesvol verzonden.</div>
-                :
-                <form method="post"
-                      onSubmit={(e) => handleSubmit(e, setFormSuccess)}
-                      className="flex flex-col lg:grid lg:grid-flow-row lg:grid-cols-2 gap-x-8 gap-y-3">
-                    <input type="hidden" name="userGroup" value="Website contactForm"/>
-                    {data.field.map((field) => (
+            ) : (
+                <form
+                    method="post"
+                    onSubmit={(e) => handleSubmit1(e, "date_from_fn")}
+                    className="flex flex-col lg:grid lg:grid-flow-row lg:grid-cols-2 gap-x-8 gap-y-3"
+                >
+                    <input type="hidden" name="userGroup" value="Website contactForm" />
+                    {data.field.map((field) =>
                         field.type === "textarea" ? (
                             <textarea
                                 className="border-2 col-start-2 row-start-2 row-span-3 placeholder:text-white text-white py-2 px-6 border-white rounded-3xl bg-transparent"
@@ -51,22 +70,28 @@ export function ContactForm({data}) {
                                 onChange={handleInput}
                             />
                         )
-                    ))}
+                    )}
                     <select
                         className="col-start-2 col-end-2 row-start-1 border-2 appearance-none outline-none placeholder:text-white text-white py-2 px-6 border-white rounded-3xl bg-transparent"
-                        name="Fase" id="Fase" required={data.dropdownVerplicht}>
+                        name="Fase"
+                        id="Fase"
+                        required={data.dropdownVerplicht}
+                    >
                         <option defaultValue>{data.dropdownDefault}</option>
                         {data.dropdown.map((field) => (
-                            <option key={field.id} value={field.titel}>{field.titel}</option>
+                            <option key={field.id} value={field.titel}>
+                                {field.titel}
+                            </option>
                         ))}
                     </select>
                     <button
                         className="col-start-2 col-end-2 hover:bg-white hover:text-byte-blue cursor-pointer ease-in-out duration-150 p-2 border-2 border-white text-white rounded-full block"
                         type="submit"
-                    >Versturen
+                    >
+                        Versturen
                     </button>
                 </form>
-            }
+            )}
         </div>
     );
 }
--- frontend/src/components/custom/Contact/emailHandler.js
+++ frontend/src/components/custom/Contact/emailHandler.js
@@ -1,26 +1,43 @@
-'use server'
+"use server";
 import nodemailer from "nodemailer";
 
-export async function handleSubmit(e, setFormSuccess) {
-    e.preventDefault();
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        host: "smtp-relay.brevo.com",
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: process.env.BREVO_USER, // generated brevo user
-            pass: process.env.BREVO_PW, // generated brevo password
-        },
-    });
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-        from: 'info@byteklaar.be', // sender address
-        to: "info@byteklaar", // list of receivers
-        subject: "Hello ✔", // Subject line
-        text: "Hello {{ contact.FIRSTNAME }} , This is an SMTP message with customizations", // plain text body
-    });
+export async function sendForm(text) {
+    try {
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+            host: "smtp-relay.brevo.com",
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: "info@byteklaar.be", // generated brevo user
+                pass: "THD-kgw2fqa-pmy2wvn", // generated brevo password
+            },
+        });
 
-    setFormSuccess(true);
-    console.log("Message sent: %s", info.messageId);
+        // send mail with defined transport object
+        let infoSentToCustomer = await transporter.sendMail({
+            from: "info@byteklaar.be", // sender address
+            to: "bramdupont@gmail.com", // list of receivers
+            subject: "Hello ✔", // Subject line
+            text: `Customer, ${text}`, // plain text body
+        });
+
+        // send mail with defined transport object
+        let infoSentToAdmin = await transporter.sendMail({
+            from: "info@byteklaar.be", // sender address
+            to: "info@byteklaar.be", // list of receivers
+            subject: "Hello ✔", // Subject line
+            text: `Admin, ${text}`, // plain text body
+        });
+
+        if (infoSentToCustomer && infoSentToAdmin) {
+            return true;
+        } else {
+            console.error("Error sending email: ", error);
+            return false;
+        }
+    } catch (error) {
+        console.error("Error sending email: ", error);
+        throw error;
+    }
 }
